fe89022fb89e7a9f442520935ab27cbba85a6dd3
kamr <kamalraj.gupthar@oracle.com>
2020-10-10 23:32:21 +0530

    30 days of code challenge. Submissions for first 4 days.

---
+++ 30_days_of_code/Day01_DataTypes.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Read and save an integer, double, and String to your variables.
+int_input = int(input())
+double_input = float(input())
+str_input = str(input())
+
+# Print the sum of both integer variables on a new line.
+print (i+int_input)
+# Print the sum of the double variables on a new line.
+print (d+double_input)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print (s+str_input)
